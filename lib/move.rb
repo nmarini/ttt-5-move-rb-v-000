81f3f8231aeb7ef2b32533bec21beae2d80d4403
Nicolas Marini <nmarini44@gmail.com>
2018-04-17 00:37:59 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,6 @@ def input_to_index(user_input)
   user_input = user_input.to_i - 1
 end
 
-def move(board, index, value = "X")
-board[index] = value
+def move(board, index, current player = "X")
+board[index] = current player
 end
